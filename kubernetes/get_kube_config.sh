459fd0dc6637340629c2897ca9f1ba5b178d72c7
Thomas Buchner <thomas.buchner@sap.com>
2018-06-06 09:23:12 +0200

    Made these scripts executable. Added some additional error-handling to get_kube_config.sh.

--- kubernetes/get_kube_config.sh
+++ kubernetes/get_kube_config.sh
@@ -2,17 +2,32 @@
 #
 
 if [ $# -ne 2 ]; then
-    echo "Usage: get_kube_config.sh <training_id> <your_ns_id>"
-    echo "Pass the IDs given to you in the training."
-    exit 1
+	echo "Usage: get_kube_config.sh <training_id> <your_namespace_id>"
+	echo "Pass the IDs given to you in the training."
+	exit 1
+fi
+
+if [ `id -u` -eq 0 ]; then
+	echo "Please do not run this script as root."
+	exit 1
 fi
 
 BASE_URL=https://cc-admin.mo.sap.corp/userContent/k8s-trainings
 CFG_URL="$BASE_URL/training-$1/kube-configs/$2/kube.config"
 TARGET=$HOME/.kube/config
 
-mkdir -p $HOME/.kube
+TMPFILE=`mktemp --dry-run`
 
-curl -s -S -o $TARGET $CFG_URL
+curl -s -S -o $TMPFILE $CFG_URL
 
-echo "*** Copied kube config to local $TARGET"
+if [ -n "$(grep '<html>' $TMPFILE)" ]; then
+	echo "ERROR: Did not receive a valid kube.config file."
+	echo "Please check that you entered the correct training ID and participant ID."
+	rm -f $TMPFILE
+	exit 1
+fi
+
+mkdir -p `dirname $TARGET`
+mv $TMPFILE $TARGET
+
+echo "*** Successfully copied kube config to local $TARGET"
