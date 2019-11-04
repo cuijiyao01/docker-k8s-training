85e496c0df73b835925b783b5c0fabf35861c9fb
Kai-Martin Dittkrist <kai-martin.dittkrist@sap.com>
2019-11-04 12:01:50 +0100

    add check on ingress url length

--- admin/logging/setup_logging.sh
+++ admin/logging/setup_logging.sh
@@ -5,6 +5,12 @@ pushd `dirname "$BASH_SOURCE"`
 CLUSTERNAME=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' | cut -d. -f2)
 PROJECTNAME=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' | cut -d. -f3)
 
+if [ $(echo 'k.ingress.${CLUSTERNAME}.${PROJECTNAME}.shoot.canary.k8s-hana.ondemand.com' | wc -m) -gt 64 ]; then
+	echo "The short hostname for kibana is longer than 64 chars!"
+	echo "Certification of url will not work, please use a shorter cluster name!"
+	exit 1
+fi
+
 sed "s/<clustername>/$CLUSTERNAME/g" 03-kibana.yaml.template > 03-kibana.yaml
 sed -i "s/<projectname>/$PROJECTNAME/g" 03-kibana.yaml
 
